e905dea23ebcac4c9f902531b073d21ace7b4d31
Michael Wood <michael.wood@opendataservices.coop>
2019-09-10 17:14:22 +0100

    cmds list_datagetter_runs Add command to output total datagetter runs

--- datastore/db/management/commands/list_datagetter_runs.py
+++ datastore/db/management/commands/list_datagetter_runs.py
@@ -6,7 +6,18 @@ from db.models import GetterRun
 class Command(BaseCommand):
     help = "Lists all datagetter ids"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--total',
+            action='store_true',
+            help="Just output the total datagetter runs in the db",
+        )
+
     def handle(self, *args, **options):
+        if options.get('total'):
+            print("%s" % GetterRun.objects.count())
+            return
+
         print("id | datetime")
         for run in GetterRun.objects.all():
             print("%s | %s" % (run.pk, str(run)))
