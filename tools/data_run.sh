059c3460783669ac3558f1ae689c1dd650a2e446
Michael Wood <michael.wood@opendataservices.coop>
2019-09-17 20:59:09 +0100

    tools data_run Update with some additional example variables
    e.g. adding DJANGO_SETTINGS_MODULE as it is unlikely to work without a
    specific production settings defined.

--- tools/data_run.sh
+++ tools/data_run.sh
@@ -4,30 +4,40 @@
 
 ### Start CONFIG ###
 SOCKS5_PORT=1337
-SSH_SERVER=SETME
+SSH_SERVER=example.com
 DOWNLOAD_DIR=~/latest_datagetter/
-GRANTNAV_DATA_DIR=~/grant_nav_data/
-GRANTNAV_DATA_PACKAGE_DOWNLOAD_DIR=~/grant_nav_packages/
+GRANTNAV_DATA_DIR=~/grantnav_data/
+GRANTNAV_DATA_PACKAGE_DOWNLOAD_DIR=/var/www/grantnav_packages/
 DATAGETTER_THREADS=16
-MAX_TOTAL_RUNS_IN_DB=20
+export DJANGO_SETTINGS_MODULE=settings.settings_production_example
+DATASTORE=~/datastore/
+MAX_TOTAL_RUNS_IN_DB=31
 # Based on running this script each day and Keep a few extra for safety
 MAX_PACKAGE_AGE_DAYS=`expr $MAX_TOTAL_RUNS_IN_DB + 2`
 ### End CONFIG ###
 
+source $DATASTORE/.ve/bin/activate
+
+#set -e
+
 mkdir -p $DOWNLOAD_DIR
+mkdir -p $GRANTNAV_DATA_DIR
 mkdir -p $GRANTNAV_DATA_PACKAGE_DOWNLOAD_DIR
 
-source ~/datastore/.ve/bin/activate
+# Clear out old download dir contents
+rm -rf $DOWNLOAD_DIR/*
 
 echo "Start the proxy"
 ssh -D $SOCKS5_PORT -q -C -N $SSH_SERVER &
 PROXY_PID=$!
 
-cd ~/datastore/
+cd $DATASTORE
 ./datastore/manage.py set_status --what datagetter --status IN_PROGRESS
 
 echo "Running the datagetter"
-datagetter.py --threads $DATAGETTER_THREADS --socks5 socks5://localhost:$SOCKS5_PORT --data-dir $DOWNLOAD_DIR
+datagetter.py --threads $DATAGETTER_THREADS --socks5 socks5://localhost:$SOCKS5_PORT --data-dir $DOWNLOAD_DIR/data
+# Comment for quick TESTING!!
+#cp -r ~/data $DOWNLOAD_DIR/data
 
 ./datastore/manage.py set_status --what datagetter --status IDLE
 
@@ -38,7 +48,7 @@ kill -HUP $PROXY_PID
 echo "Load the downloaded datagetter data into datastore"
 ./datastore/manage.py set_status --what datastore --status LOADING_DATA
 
-./datastore/manage.py load_datagetter_data $DOWNLOAD_DIR
+./datastore/manage.py load_datagetter_data $DOWNLOAD_DIR/data
 
 ./datastore/manage.py set_status --what datastore --status IDLE
 
@@ -46,12 +56,15 @@ echo "Create grant nav package"
 ./datastore/manage.py set_status --what grantnav_data_package --status LOADING_DATA
 
 # Remove old data dump
-rm -rf $GRANTNAV_DATA_DIR || true
-./datastore/manage.py create_datagetter_data --dir $GRANTNAV_DATA_DIR
+rm -rf $GRANTNAV_DATA_DIR/data || true
+./datastore/manage.py create_datagetter_data --dir $GRANTNAV_DATA_DIR/data
 
 NEW_PACKAGE_NAME=data_$(date +%F).tar.gz
 
-tar -czf $GRANTNAV_DATA_PACKAGE_DOWNLOAD_DIR/$NEW_PACKAGE_NAME $GRANTNAV_DATA_DIR
+cd $GRANTNAV_DATA_DIR
+tar -czf $GRANTNAV_DATA_PACKAGE_DOWNLOAD_DIR/$NEW_PACKAGE_NAME data
+# go back to original dir
+cd $DATASTORE
 
 # Create latest_grantnav_data.tar.gz symlink
 rm $GRANTNAV_DATA_PACKAGE_DOWNLOAD_DIR/latest_grantnav_data.tar.gz
