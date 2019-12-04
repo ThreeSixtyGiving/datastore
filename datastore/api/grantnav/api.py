064ff519c47d83ac47cd776cebf322607c19ce60
Michael Wood <michael.wood@opendataservices.coop>
2019-12-04 17:03:59 +0000

    api grantnav Remoave redundant api
    We ended up not using this API so remove it to avoid confusion.

--- datastore/api/grantnav/api.py
+++ datastore/api/grantnav/api.py
@@ -1,38 +1,7 @@
-from django.http.response import StreamingHttpResponse, JsonResponse
+from django.http.response import JsonResponse
 from django.views import View
 import db.models as db
 from django.conf import settings
-
-import json
-
-
-class GrantNavApiView(View):
-    """ Special API endpoint to stream data to GrantNav """
-
-    def get(self, *args, **kwargs):
-        print(self.request.GET)
-        limit = int(self.request.GET.get('limit', 400))
-
-        def output_generator():
-            grants = db.Grant.objects.all()[:limit]
-
-            yield b'['
-
-            for i, grant in enumerate(grants):
-                data_item = {
-                    'publisher': grant.publisher.data,
-                    'grant': grant.data,
-                }
-
-                yield json.dumps(data_item)
-
-                if i < (len(grants)-1):
-                    yield ','
-
-            yield b']'
-
-        return StreamingHttpResponse(output_generator(),
-                                     content_type='text/json; charset=UTF-8')
 
 
 class GrantNavPollForNewData(View):
