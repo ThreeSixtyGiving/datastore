d42bbbf588f5220950741284a40454a4765b7fdd
Michael Wood <michael.wood@opendataservices.coop>
2020-04-27 11:09:53 +0100

    OrgInfoCache: Add FindThatCharity data source for additional_data
    This data source is sourced from FindThatCharity data and contains various
    useful organisation info that is added to help provide more context to
    information about recipient organisations.
    
    Implements https://github.com/ThreeSixtyGiving/datastore/issues/28

--- datastore/additional_data/generator.py
+++ datastore/additional_data/generator.py
@@ -1,3 +1,4 @@
+from additional_data.sources.find_that_charity import FindThatCharitySource
 from additional_data.sources.local_files import LocalFilesSource
 
 
@@ -6,6 +7,7 @@ class AdditionalDataGenerator(object):
 
     def __init__(self):
         self.local_files_source = LocalFilesSource()
+        self.find_that_charity_source = FindThatCharitySource()
         # Initialise Other Sources heres
 
     def create(self, grant):
@@ -16,5 +18,6 @@ class AdditionalDataGenerator(object):
         # This ordering is important for any data dependencies
         # Add other additional_data updaters here
         self.local_files_source.update_additional_data(grant, additional_data)
+        self.find_that_charity_source.update_additional_data(grant, additional_data)
 
         return additional_data
---
+++ datastore/additional_data/sources/find_that_charity.py
@@ -0,0 +1,121 @@
+import csv
+import json
+
+import requests
+
+from additional_data.models import OrgInfoCache
+
+
+class FindThatCharitySource(object):
+    """ This is responsible for inputting and outputting data from the
+    FindThatCharity (FTC) organisation info data sources """
+
+    ADDITIONAL_DATA_KEY = "recipientOrgInfos"
+
+    def __init__(self, *args, **kwargs):
+        # A basic internal memory cache to avoid hitting the db on duplicate
+        # requests. Vastly speeds this process up.
+        # OrgInfoCache db typical size is 565,110
+        # This cache object typical size 68,458
+        self._cache = {}
+
+    def update_additional_data(self, grant, additional_data):
+        if "id" not in grant["recipientOrganization"][0]:
+            return
+
+        org_id = grant["recipientOrganization"][0]["id"]
+
+        if "360G-" in org_id:
+            # Not valid org-id
+            return
+
+        # Restart the cache after 300,000 this is approximately
+        # 137.6MiB of memory.
+        if len(self._cache.keys()) > 300000:
+            self._cache = {}
+
+        try:
+            try:
+                # Memory cache because a lot of these are going to be the same
+                org_infos = self._cache[org_id]
+                additional_data[self.ADDITIONAL_DATA_KEY] = org_infos
+            except KeyError:
+                org_infos = OrgInfoCache.objects.filter(org_ids__contains=[org_id])
+                org_infos = list(
+                    OrgInfoCache.objects.filter(org_ids__contains=[org_id]).values_list(
+                        "data", flat=True
+                    )
+                )
+
+                additional_data[self.ADDITIONAL_DATA_KEY] = org_infos
+                self._cache[org_id] = org_infos
+        except OrgInfoCache.DoesNotExist:
+            # Store no hit so that we don't bother the db for impossible queries
+            self._cache[org_id] = None
+
+    def process_csv(self, file_data, org_type):
+        """ Returns total added. file_data array from csv """
+        added = 0
+        bulk_list = []
+
+        for row in file_data:
+            # Re-write string array "[ 'a','b','c' ]" values in the csv into arrays
+            for key in row:
+                if type(row[key]) == str and "[" in row[key]:
+                    # Use json parser to turn the values into an array
+                    # this could also be done by eval(val) but that is a little scary.
+                    # The data also uses single quotes ['a'] to avoid csv breaking so
+                    # those have to be replaced to be valid json.
+                    try:
+                        row[key] = json.loads(
+                            '{ "a" : %s }' % row[key].replace("'", '"')
+                        )["a"]
+                    except json.decoder.JSONDecodeError:
+                        # This can be incorrectly triggered via values with square brackets in
+                        # e.g. A name [with brackets] Association
+                        continue
+
+            if "orgIDs" not in row:
+                row["orgIDs"] = None
+
+            bulk_list.append(
+                OrgInfoCache(
+                    data=row, org_type=org_type, org_id=row["id"], org_ids=row["orgIDs"]
+                )
+            )
+            added += 1
+
+        OrgInfoCache.objects.bulk_create(bulk_list)
+
+        return added
+
+    def import_from_path(self, path, org_type=None):
+        """ Path can be http or file path, org_type if omitted we guess from the filename """
+        added = 0
+
+        # Have a guess at the org type from the path
+        if not org_type:
+            for (short_type, long_type) in OrgInfoCache.ORG_TYPE:
+                if short_type in path:
+                    org_type = short_type
+                    print("Guessed org_type %s" % org_type)
+                    break
+
+        if not org_type:
+            raise self.OrgTypeNotKnownError
+
+        if "http" in path:
+            with requests.get(path, stream=True) as r:
+                file_data = csv.DictReader(
+                    r.iter_lines(decode_unicode=True), delimiter=","
+                )
+                added = self.process_csv(file_data, org_type)
+        else:
+            with open(path) as csv_file:
+                file_data = csv.DictReader(csv_file, delimiter=",")
+                added = self.process_csv(file_data, org_type)
+
+        return added
+
+    class OrgTypeNotKnownError(Exception):
+        pass
