588bd102372b7ff3baeca41d3daff5b313649dd8
Michael Wood <michael.wood@opendataservices.coop>
2020-09-16 16:05:59 +0100

    additional_data: Add command to rewrite the additional data field
    Adds a command to re-write the additional_data object on a set of grant
    data. This is useful for development, normally the additional_data is
    written to only at the time of loading the grants.

---
+++ datastore/additional_data/management/commands/rewrite_additional_data.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+
+from additional_data.generator import AdditionalDataGenerator
+from db.models import Grant, Latest
+
+
+class Command(BaseCommand):
+    help = (
+        "Reloads the additional data on grant data specified by datagetter id or latest"
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            type=str,
+            action="store",
+            dest="getter_run",
+            help="The datagetter run id or latest",
+        )
+
+    def handle(self, *args, **options):
+
+        if "latest" in options["getter_run"]:
+            grants = Latest.objects.get(series=Latest.CURRENT).grant_set.all()
+        else:
+            grants = Grant.objects.filter(getter_run=options["getter_run"])
+
+        generator = AdditionalDataGenerator()
+
+        for grant in grants:
+            additional_data = generator.create(grant.data)
+            grant.additional_data = additional_data
+            grant.save()
