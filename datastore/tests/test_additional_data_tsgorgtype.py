f1dc5880ec658bf6c952d3f27485fda70de3e048
Michael Wood <michael.wood@opendataservices.coop>
2020-09-16 15:23:03 +0100

    tests: Add test for additional_data tsg org types
    Adds basic unit test for ThreeSixtyGiving org types in additional data

---
+++ datastore/tests/test_additional_data_tsgorgtype.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+
+from additional_data.models import TSGOrgType
+from additional_data.sources.tsg_org_types import TSGOrgTypesSource
+from db.models import Grant
+
+
+class TestTSGOrgTypeAdditionalData(TestCase):
+    fixtures = ["test_data.json", "default_tsg_org_types.json"]
+
+    def test_update_additional_data_low_priority(self):
+        tsg_org_types = TSGOrgTypesSource()
+        grant = Grant.objects.last()
+
+        grant.data["fundingOrganization"][0]["id"] = "ABC-1234"
+
+        additional_data = {}
+
+        tsg_org_types.update_additional_data(grant.data, additional_data)
+
+        lowest_priority_org_type = (
+            TSGOrgType.objects.order_by("priority").last().tsg_org_type
+        )
+
+        self.assertTrue(
+            lowest_priority_org_type
+            in additional_data[TSGOrgTypesSource.ADDITIONAL_DATA_KEY],
+            "Expected %s" % lowest_priority_org_type,
+        )
+
+    def test_update_additional_data_multiple_rules(self):
+        # National lottery but also GOR
+        # Should prioritise national lottery over government
+        tsg_org_types = TSGOrgTypesSource()
+        grant = Grant.objects.last()
+        grant.data["fundingOrganization"][0]["id"] = "GB-GOR-PC390"
+
+        additional_data = {}
+        tsg_org_types.update_additional_data(grant.data, additional_data)
+
+        self.assertTrue(
+            "National Lottery"
+            in additional_data[TSGOrgTypesSource.ADDITIONAL_DATA_KEY],
+            "Expected National Lottery",
+        )
