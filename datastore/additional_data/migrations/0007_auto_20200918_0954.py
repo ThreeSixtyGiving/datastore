5f536feb2e174019476b6a0fd50b9c764bbc1ef2
David Kane <david@dkane.net>
2020-09-18 11:04:49 +0100

    run black

--- datastore/additional_data/migrations/0007_auto_20200918_0954.py
+++ datastore/additional_data/migrations/0007_auto_20200918_0954.py
@@ -6,13 +6,16 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('additional_data', '0006_tsgorgtype'),
+        ("additional_data", "0006_tsgorgtype"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='tsgorgtype',
-            name='priority',
-            field=models.IntegerField(help_text='Which pattern will take precedence (Higher number = higher priority)', unique=True),
+            model_name="tsgorgtype",
+            name="priority",
+            field=models.IntegerField(
+                help_text="Which pattern will take precedence (Higher number = higher priority)",
+                unique=True,
+            ),
         ),
     ]
