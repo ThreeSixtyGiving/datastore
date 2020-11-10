32ea8753c4294c8853f44ff50e24e92f4539fc78
David Kane <david.kane@threesixtygiving.org>
2020-11-10 15:20:31 +0000

    additional_data: org_info: Update findthatcharity URL
    Findthatcharity has now been upgraded so `ftc.ftc.dkane.net` has been
    replaced by `findthatcharity.uk`.

--- datastore/additional_data/sources/load_all_org_data.sh
+++ datastore/additional_data/sources/load_all_org_data.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-# Load all the org data from dkane's ftc site
+# Load all the different types of org data from Find That Charity.
 
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/casc.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/ccew.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/ccni.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/oscr.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/companies.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/mutuals.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/gor.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/grid.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/hesa.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/lae.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/lani.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/las.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/pla.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/nhsods.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/rsl.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/schools_gias.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/schools_ni.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/schools_scotland.csv
-./manage.py load_org_data https://ftc.ftc.dkane.net/orgid/source/schools_wales.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/casc.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/ccew.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/ccni.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/oscr.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/companies.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/mutuals.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/gor.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/grid.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/hesa.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/lae.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/lani.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/las.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/pla.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/nhsods.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/rsl.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/schools_gias.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/schools_ni.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/schools_scotland.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/schools_wales.csv
