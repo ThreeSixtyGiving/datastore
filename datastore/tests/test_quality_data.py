147723d5ed6a5106365f7002e8d8324b33adf44d
Michael Wood <michael.wood@opendataservices.coop>
2021-05-25 17:59:53 +0100

    tests: Add test_quality_data
    Basic test to check that generating the quality data works

---
+++ datastore/tests/test_quality_data.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+from data_quality import quality_data
+import db.models as db
+import json
+
+
+class TestDataQualityData(TestCase):
+    """Test creating data quality data"""
+
+    fixtures = ["test_data.json"]
+
+    def test_create_data_quality_data(self):
+        grant = db.Grant.objects.first()
+        quality = quality_data.create({"grants": [grant.data]})
+
+        # Our test data in the test_data.json currently generates 3
+        # data quality usefulness results
+        self.assertEqual(len(quality), 3)
