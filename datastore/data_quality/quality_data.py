d3da92a9989dc89b0a1e6fcb06745a8413c86f99
Michael Wood <michael.wood@opendataservices.coop>
2021-05-26 16:23:52 +0100

    data_quality: quality_data: Fill-in 0 values when we have passing tests

--- datastore/data_quality/quality_data.py
+++ datastore/data_quality/quality_data.py
@@ -1,5 +1,6 @@
 from lib360dataquality.cove.threesixtygiving import (
     USEFULNESS_TEST_CLASS,
+    TEST_CLASSES,
     common_checks_360,
 )
 from lib360dataquality.cove.schema import Schema360
@@ -20,20 +21,24 @@ def create(grants):
 
     # We don't quite want the result as-is
     # our format is:
-    # [
-    #   { "TestClassName": { "heading": "text", "count": n } }
-    # ]
+    # {
+    #   "TestClassName": { "heading": "text", "count": n },
+    #   "TestTwoClassName": { "count": 0 },
+    # }
+    # We only get results when a quality test finds an issue, so to provide a count 0
+    # value we template out all the expected results from all the available tests.
 
-    quality_results = []
+    quality_results = {}
 
+    # Create the template
+    for available_test in TEST_CLASSES[USEFULNESS_TEST_CLASS]:
+        quality_results[available_test.__name__] = {"count": 0}
+
+    # Update with the results
     for test in cove_results["usefulness_checks"]:
-        quality_results.append(
-            {
-                test[0]["type"]: {
-                    "heading": test[0]["heading"],
-                    "count": test[0]["count"],
-                }
-            }
-        )
+        quality_results[test[0]["type"]] = {
+            "heading": test[0]["heading"],
+            "count": test[0]["count"],
+        }
 
     return quality_results
