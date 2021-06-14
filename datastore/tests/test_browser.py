feada8627d76bf27ec07aac9c2c31ba2c8197887
Michael Wood <michael.wood@opendataservices.coop>
2021-06-14 16:03:47 +0100

    requirements_dev/test_browser: Add chromedriver_autoinstaller

--- datastore/tests/test_browser.py
+++ datastore/tests/test_browser.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
+import chromedriver_autoinstaller
+
+chromedriver_autoinstaller.install()
+
 
 class BrowserTests(StaticLiveServerTestCase):
     """Browser test using latest Chrome/Chromium stable"""
