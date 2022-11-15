c68c445ba04d7341ad045eed76d12e6e7641fea1
Michael Wood <michael.wood@opendataservices.coop>
2022-11-15 19:06:20 +0000

    db/additional_data: Swap JSONField to new django model field
    The old contrib is now deprecated.

--- datastore/db/models.py
+++ datastore/db/models.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 from django.db import connection, models
 from django.db.utils import DataError
 from django.utils import timezone
