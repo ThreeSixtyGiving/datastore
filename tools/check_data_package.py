ccfec82e20df76daca87dfd115dd5405965e97c6
Michael Wood <michael.wood@opendataservices.coop>
2023-01-19 16:11:53 +0000

    tools: Move the datastore output package checker tool to this repo

---
+++ tools/check_data_package.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import os
+import smtplib
+import requests
+from datetime import datetime
+
+package_url = "https://store.data.threesixtygiving.org/grantnav_packages/latest_grantnav_data.tar.gz"
+try:
+    to = os.environ["TO"]
+    from_ = os.environ["FROM"]
+except KeyError:
+    print("Error: Environment variables TO or FROM not set")
+    exit()
+
+
+def send_alert(text):
+    header = "To:%s\nFrom:%s\nSubject:WARNING: 360G Datastore Data Package issue\n" % (
+        to,
+        from_,
+    )
+
+    content = header + "\n\n" + text
+
+    mail = smtplib.SMTP(
+        os.environ.get("SMTP_HOST", "localhost"),
+        os.environ.get("SMTP_PORT", 25),
+    )
+
+    mail.ehlo()
+    mail.starttls()
+
+    mail.sendmail(from_, to, content)
+    mail.quit()
+
+
+# Script Checks the 360 datastore to see if a data package has been generated today
+def main():
+    try:
+        r = requests.head(
+            package_url, auth=(os.environ.get("USER"), os.environ.get("PASSWORD"))
+        )
+        r.raise_for_status()
+
+        pkg_modified = datetime.strptime(
+            r.headers["Last-Modified"], "%a, %d %b %Y %H:%M:%S %Z"
+        )
+
+        today = datetime.now()
+
+        str_pkg_mod = pkg_modified.strftime("%Y-%m-%d")
+        str_today = today.strftime("%Y-%m-%d")
+
+        if str_today != str_pkg_mod:
+            send_alert(
+                "There is no latest_grantnav_data package available for today (%s). Latest available is %s"
+                % (str_today, str_pkg_mod)
+            )
+
+    except Exception as e:
+        send_alert(
+            "Error checking the datastore for the latest data package at %s"
+            % package_url
+        )
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
