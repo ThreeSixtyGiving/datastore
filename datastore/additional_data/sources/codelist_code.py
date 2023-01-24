590867a533073e06309e9a874d987ea9371098d7
Michael Wood <michael.wood@opendataservices.coop>
2023-01-24 13:39:11 +0000

    additional_data: Add module to lookup title field for codelists

---
+++ datastore/additional_data/sources/codelist_code.py
@@ -0,0 +1,90 @@
+import csv
+import requests
+
+from additional_data.models import CodelistCode
+
+code_lists_urls = [
+    "https://raw.githubusercontent.com/ThreeSixtyGiving/standard/master/codelists/grantToIndividualsPurpose.csv",
+    "https://raw.githubusercontent.com/ThreeSixtyGiving/standard/master/codelists/grantToIndividualsReason.csv",
+    "https://raw.githubusercontent.com/ThreeSixtyGiving/standard/master/codelists/regrantType.csv",
+    # These lists aren't yet ready for use in the datastore
+    # https://github.com/ThreeSixtyGiving/standard/issues/348
+    # https://github.com/ThreeSixtyGiving/standard/issues/349
+    # "https://raw.githubusercontent.com/ThreeSixtyGiving/standard/master/codelists/countryCode.csv",
+    # "https://raw.githubusercontent.com/ThreeSixtyGiving/standard/master/codelists/currency.csv",
+    # "https://raw.githubusercontent.com/ThreeSixtyGiving/standard/master/codelists/geoCodeType.csv",
+]
+
+
+class CodeListSource(object):
+    """Looks up codes from 360Giving codelists and gets the title value of the code
+    responsible for field: codeListLookup
+    """
+
+    def import_codelists(self):
+        CodelistCode.objects.all().delete()
+
+        for code_list_url in code_lists_urls:
+            # list name = last item in split -4 to remove extension .csv
+            list_name = code_list_url.split("/")[-1:][0][:-4]
+            with requests.get(code_list_url, stream=True) as r:
+                r.raise_for_status()
+                file_data = csv.DictReader(
+                    r.iter_lines(decode_unicode=True), delimiter=","
+                )
+                for value in file_data:
+                    CodelistCode.objects.create(
+                        code=value["Code"],
+                        title=value["Title"],
+                        description=value["Description"],
+                        list_name=list_name,
+                    )
+
+    def update_additional_data(self, grant, additional_data):
+        # check All the fields in the grant data that use codelists and make additional data field versions of them
+
+        primaryGrantReason = ""
+        secondaryGrantReason = ""
+        grantPurpose = ""
+        regrantType = ""
+
+        try:
+            code = grant["toIndividualDetails"]["primaryGrantReason"]
+            primaryGrantReason = CodelistCode.objects.get(
+                code=code, list_name="grantToIndividualsReason"
+            ).title
+        except (KeyError, CodelistCode.DoesNotExist):
+            pass
+
+        try:
+            code = grant["toIndividualDetails"]["secondaryGrantReason"]
+            secondaryGrantReason = CodelistCode.objects.get(
+                code=code, list_name="grantToIndividualsReason"
+            ).title
+        except (KeyError, CodelistCode.DoesNotExist):
+            pass
+
+        try:
+            code = grant["toIndividualDetails"]["grantPurpose"]
+            grantPurpose = CodelistCode.objects.get(
+                code=code, list_name="grantToIndividualsPurpose"
+            ).title
+        except (KeyError, CodelistCode.DoesNotExist):
+            pass
+
+        try:
+            code = grant["regrantType"]
+            regrantType = CodelistCode.objects.get(
+                code=code, list_name="regrantType"
+            ).title
+        except (KeyError, CodelistCode.DoesNotExist):
+            pass
+
+        additional_data["codeListLookup"] = {
+            "toIndividualDetails": {
+                "primaryGrantReason": primaryGrantReason,
+                "secondaryGrantReason": secondaryGrantReason,
+                "grantPurpose": grantPurpose,
+            },
+            "regrantType": regrantType,
+        }
