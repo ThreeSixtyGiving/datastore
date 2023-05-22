f9adb117eaacfa07372949cd0215ce8657b36d02
Michael Wood <michael.wood@opendataservices.coop>
2023-05-22 16:50:21 +0100

    additional_data: Remove local_files
    Deprecate old Canonical recipient/funder mechanism

--- datastore/additional_data/generator.py
+++ datastore/additional_data/generator.py
@@ -1,6 +1,5 @@
 from additional_data.sources.find_that_charity import FindThatCharitySource
 from additional_data.sources.geo_lookup import GeoLookupSource
-from additional_data.sources.local_files import LocalFilesSource
 from additional_data.sources.nspl import NSPLSource
 from additional_data.sources.tsg_org_types import TSGOrgTypesSource
 from additional_data.sources.additional_data_recipient_location import (
@@ -14,7 +13,6 @@ class AdditionalDataGenerator(object):
     """Adds additional data to grant data"""
 
     def __init__(self):
-        self.local_files_source = LocalFilesSource()
         self.find_that_charity_source = FindThatCharitySource()
         self.nspl_source = NSPLSource()
         self.geo_lookup = GeoLookupSource()
@@ -31,7 +29,6 @@ class AdditionalDataGenerator(object):
 
         # This ordering is important for any data dependencies
         # Add other additional_data updaters here
-        self.local_files_source.update_additional_data(grant, additional_data)
         self.find_that_charity_source.update_additional_data(grant, additional_data)
         self.nspl_source.update_additional_data(grant, additional_data)
         self.geo_lookup.update_additional_data(grant, additional_data)
