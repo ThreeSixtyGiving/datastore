216f13f5400d95c85662f3b329315d4a327a82fe
Rikki Guy <rikki.guy@opendataservices.coop>
2023-08-07 09:58:32 +0100

    api: Add experimental organisation API

--- datastore/api/experimental/serializers.py
+++ datastore/api/experimental/serializers.py
@@ -7,3 +7,15 @@ class GrantSerializer(serializers.ModelSerializer):
     class Meta:
         model = db.Grant
         fields = "__all__"
+
+
+class FunderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = db.Funder
+        fields = "__all__"
+
+
+class RecipientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = db.Recipient
+        fields = "__all__"
