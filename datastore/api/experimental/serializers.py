3fe2d0a68cb9fb0d0c2a28adeb1bd9e8764613d2
Rikki Guy <rikki.guy@opendataservices.coop>
2023-08-15 14:34:48 +0100

    api: Add serialiser for Publisher

--- datastore/api/experimental/serializers.py
+++ datastore/api/experimental/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 import db.models as db
 
 
+class GetterRunSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = db.GetterRun
+        fields = ["datetime", "archived"]
+
+
 class GrantSerializer(serializers.ModelSerializer):
     class Meta:
         model = db.Grant
@@ -19,3 +25,12 @@ class RecipientSerializer(serializers.ModelSerializer):
     class Meta:
         model = db.Recipient
         fields = "__all__"
+
+class PublisherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = db.Publisher
+        exclude = ["id"]
+
+    getter_run = GetterRunSerializer()
+
+
