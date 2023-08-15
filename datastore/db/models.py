f5e010632e3857f74a5696a030843aa9d9f0a6bc
Rikki Guy <rikki.guy@opendataservices.coop>
2023-08-15 15:26:21 +0100

    api: Add unique constraint to Funder and Recipient

--- datastore/db/models.py
+++ datastore/db/models.py
@@ -327,11 +327,17 @@ class Publisher(Entity):
 
 
 class Recipient(Entity):
-    pass
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["org_id"], name="recipient_unique_org_id")
+        ]
 
 
 class Funder(Entity):
-    pass
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["org_id"], name="funder_unique_org_id")
+        ]
 
 
 class Grant(models.Model):
