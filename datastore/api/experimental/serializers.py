78a39006e672bf67b4018d972ff1574b20cb6e94
Rikki Guy <rikki.guy@opendataservices.coop>
2023-10-19 16:58:38 +0100

    api: Add self ref to org list view

--- datastore/api/experimental/serializers.py
+++ datastore/api/experimental/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 import db.models as db
 
@@ -36,8 +37,16 @@ class PublisherSerializer(serializers.ModelSerializer):
 
 
 class OrganisationListSerializer(serializers.Serializer):
+    self = serializers.SerializerMethodField()
     org_id = serializers.CharField(max_length=200)
     name = serializers.CharField(allow_blank=True, required=False)
+
+    def get_self(self, org):
+        """Get the URL to this object's detail."""
+        request = self.context.get("request")
+        return reverse(
+            "api:organisation-detail", kwargs={"org_id": org["org_id"]}, request=request
+        )
 
 
 class OrganisationSerializer(serializers.Serializer):
