fae10c92ac7284ffbd5951e370967d7d172bba48
Rikki Guy <rikki.guy@opendataservices.coop>
2023-11-06 18:47:03 +0000

    dev: Add stable SECRET_KEY
    Make the SECRET_KEY in settings_dev stable, so that you don't lose your
    login/session each time the dev server reloads.

--- datastore/settings/settings_dev.py
+++ datastore/settings/settings_dev.py
@@ -1,9 +1,26 @@
 # flake8: noqa
+<<<<<<< Updated upstream
+=======
+import os
+import socket
+import hashlib
+>>>>>>> Stashed changes
 from settings.settings import *
 
 # This adds the CORS header to API calls for the django dev server
 
 DEBUG = True
+
+
+# Use combo of hostname+cwd to generate secret key, so that it's stable across reloads
+def get_secret_key():
+    secret_key_m = hashlib.sha256()
+    secret_key_m.update(socket.gethostname().encode())
+    secret_key_m.update(os.getcwd().encode())
+    return secret_key_m.digest()
+
+
+SECRET_KEY = get_secret_key()
 
 INSTALLED_APPS = INSTALLED_APPS + ["corsheaders"]
 
