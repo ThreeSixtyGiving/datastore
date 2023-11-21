3dcc680f10ccc16813d9411564e1d983831e7811
Rikki Guy <rikki.guy@opendataservices.coop>
2023-11-21 13:45:35 +0000

    additional_data: Update NHSODS org data URLs
    Replace old nhsods with individual sources.

--- datastore/additional_data/sources/load_all_org_data.sh
+++ datastore/additional_data/sources/load_all_org_data.sh
@@ -14,7 +14,14 @@
 ./manage.py load_org_data https://findthatcharity.uk/orgid/source/lani.csv
 ./manage.py load_org_data https://findthatcharity.uk/orgid/source/las.csv
 ./manage.py load_org_data https://findthatcharity.uk/orgid/source/pla.csv
-./manage.py load_org_data https://findthatcharity.uk/orgid/source/nhsods.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/nhsods-epraccur.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/nhsods-etr.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/nhsods-ensa.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/nhsods-eccg.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/nhsods-ecsu.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/nhsods-espha.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/nhsods-wlhb.csv
+./manage.py load_org_data https://findthatcharity.uk/orgid/source/nhsods-ect.csv
 ./manage.py load_org_data https://findthatcharity.uk/orgid/source/rsl.csv
 ./manage.py load_org_data https://findthatcharity.uk/orgid/source/gias.csv schools_gias
 ./manage.py load_org_data https://findthatcharity.uk/orgid/source/nideptofeducation.csv schools_ni
