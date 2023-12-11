d613ff1c759c72d9a96350e6b892d1820b7ff4fc
Rikki Guy <rikki.guy@opendataservices.coop>
2023-12-11 20:19:34 +0000

    api: Add Organisation.get method
    Move the logic to create an Organisation "fake" model instance from db, out of
    the view and into the model itsself.

--- datastore/api/experimental/models.py
+++ datastore/api/experimental/models.py
@@ -1,5 +1,7 @@
 from typing import Optional
 from dataclasses import dataclass
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.db.models.query import QuerySet
 
 import db.models as db
 
@@ -31,3 +33,65 @@ class Organisation:
     def __post_init__(self):
         if self.org_id == "":
             raise ValueError("org_id cannot be empty string")
+
+    # Add Model.DoesNotExist and MultipleObjectsReturned exceptions to mimic Django models
+    class DoesNotExist(ObjectDoesNotExist):
+        pass
+
+    class MultipleObjectsReturned(MultipleObjectsReturned):
+        pass
+
+    @staticmethod
+    def get(
+        org_id: str,
+        funder_queryset: Optional[QuerySet[db.Funder]] = None,
+        recipient_queryset: Optional[QuerySet[db.Recipient]] = None,
+        publisher_queryset: Optional[QuerySet[db.Publisher]] = None,
+    ) -> "Organisation":
+        """
+        Retrieve a single Organisation by org_id.
+        This combines queries of the Funder, Recipient and Publisher sub-models.
+
+        By default will query the current set of each sub-model, but each queryset can be overridden by passing the *_queryset kwargs.
+
+        Returns an instance of Organisation if found, otherwise raises Organisation.DoesNotExist if not found.
+        """
+        if not funder_queryset:
+            funder_queryset = db.Funder.objects.all()
+
+        if not recipient_queryset:
+            recipient_queryset = db.Recipient.objects.all()
+
+        if not publisher_queryset:
+            publisher_queryset = db.Publisher.objects.filter(
+                getter_run__in=db.GetterRun.objects.in_use()
+            )
+
+        # is org a Funder?
+        try:
+            funder = funder_queryset.get(org_id=org_id)
+
+        except db.Funder.DoesNotExist:
+            funder = None
+
+        # is org a Recipient?
+        try:
+            recipient = recipient_queryset.get(org_id=org_id)
+
+        except db.Recipient.DoesNotExist:
+            recipient = None
+
+        # is org a Publisher?
+        try:
+            publisher = publisher_queryset.filter(org_id=org_id).order_by(
+                "-getter_run__datetime"
+            )[0]
+        except IndexError:
+            publisher = None
+
+        if funder is None and recipient is None and publisher is None:
+            raise Organisation.DoesNotExist
+
+        return Organisation(
+            org_id=org_id, funder=funder, recipient=recipient, publisher=publisher
+        )
