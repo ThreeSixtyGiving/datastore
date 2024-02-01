76f2078ba568812a808fef37b38cc88d1b4f2dec
Rikki Guy <rikki.guy@opendataservices.coop>
2024-02-01 17:15:33 +0000

    api: Add 360G Schema converted to OpenAPI
    and restrict the schema/swagger-ui to display only public endpoints.

---
+++ datastore/api/org/api_schema.py
@@ -0,0 +1,37 @@
+from drf_spectacular.views import SpectacularAPIView
+
+
+INCLUDE_PATHS = set(
+    [
+        "/api/experimental/org/{org_id}/grants_made/",
+        "/api/experimental/org/{org_id}/grants_received/",
+        "/api/experimental/org/{org_id}/",
+        "/api/experimental/org/",
+        "/api/experimental/grant/{grant_id}/",
+    ]
+)
+
+
+def only_include_org_api_endpoints(endpoints):
+    """
+    Only keep endpoints in the schema which are relevent to the Org Grants API.
+    """
+    kept_endpoints = [
+        (path, path_regex, method, callback)
+        for path, path_regex, method, callback in endpoints
+        if path in INCLUDE_PATHS
+    ]
+    return kept_endpoints
+
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "360G Organisation Grants API",
+    "DESCRIPTION": "Describe grants made and recieved by organisations.",
+    "VERSION": "0.0.1",
+    "COMPONENT_SPLIT_REQUEST": True,
+    "PREPROCESSING_HOOKS": [__name__ + "." + only_include_org_api_endpoints.__name__],
+}
+
+
+class APISchemaView(SpectacularAPIView):
+    custom_settings = SPECTACULAR_SETTINGS
