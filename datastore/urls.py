e468ce3e63d15c7afe58108d0128e274dac5737a
Rikki Guy <rikki.guy@opendataservices.coop>
2024-02-23 14:39:51 +0000

    dev: Add Django Debug Toolbar
    Add the Django Debug Toolbar to the project, in particular to make debugging SQL
    queries easier.

--- datastore/urls.py
+++ datastore/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 # This is the root urlconf
 
@@ -28,3 +30,6 @@ urlpatterns = [
     ),
     path("admin/", admin.site.urls, name="admin"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
