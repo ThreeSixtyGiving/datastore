6577dc8b8392ac02ebe07d9bd5033a3e122d2560
Michael Wood <michael.wood@opendataservices.coop>
2024-03-07 16:09:48 +0000

    additional_data: rewrite command: Add a method to specify sources
    When testing and working with additional_data sources we sometimes only
    want to rewrite the contents that is generated by one particular
    additional_data source.

--- datastore/additional_data/management/commands/rewrite_additional_data.py
+++ datastore/additional_data/management/commands/rewrite_additional_data.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from additional_data.generator import AdditionalDataGenerator
+from additional_data.generator import AdditionalDataGenerator, DATA_SOURCES
 from db.models import Grant, Latest
 
 
@@ -17,6 +17,16 @@ class Command(BaseCommand):
             help="The datagetter run id or latest",
         )
 
+        parser.add_argument(
+            "--data-sources",
+            type=str,
+            action="store",
+            dest="data_sources",
+            nargs="*",
+            default=None,
+            help=f"Customise the data sources to used to update additional_data. Available sources: {DATA_SOURCES}",
+        )
+
     def handle(self, *args, **options):
 
         if "latest" in options["getter_run"]:
@@ -27,7 +37,11 @@ class Command(BaseCommand):
         generator = AdditionalDataGenerator()
 
         for grant in grants:
-            additional_data = generator.create(grant.data)
+            if options["data_sources"]:
+                additional_data = generator.create(grant.data, options["data_sources"])
+            else:
+                additional_data = generator.create(grant.data)
+
             grant.additional_data = additional_data
 
         Grant.objects.bulk_update(grants, ["additional_data"], batch_size=10000)
