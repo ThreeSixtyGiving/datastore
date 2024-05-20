1d1a1d5427c9feeefb1a88127c0a3b053d510f10
Rikki Guy <rikki.guy@opendataservices.coop>
2024-05-20 14:13:13 +0100

    additional_data: Rewrite Additional Data in Chunks
    Update the rewrite_additional_data command to update Grants in chunks so
    as to use less memory.

--- datastore/additional_data/management/commands/rewrite_additional_data.py
+++ datastore/additional_data/management/commands/rewrite_additional_data.py
@@ -1,7 +1,14 @@
+import logging
+
 from django.core.management.base import BaseCommand
+from django.core.paginator import Paginator
 
 from additional_data.generator import AdditionalDataGenerator, DATA_SOURCES
 from db.models import Grant, Latest
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_CHUNK_SIZE = 10_000
 
 
 class Command(BaseCommand):
@@ -18,6 +25,14 @@ class Command(BaseCommand):
         )
 
         parser.add_argument(
+            "--grants-chunk-size",
+            type=int,
+            action="store",
+            default=DEFAULT_CHUNK_SIZE,
+            help="Number of Grants to update simultaneously",
+        )
+
+        parser.add_argument(
             "--data-sources",
             type=str,
             action="store",
@@ -28,20 +43,36 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-
         if "latest" in options["getter_run"]:
             grants = Latest.objects.get(series=Latest.CURRENT).grant_set.all()
         else:
             grants = Grant.objects.filter(getter_run=options["getter_run"])
 
         generator = AdditionalDataGenerator()
+        data_sources = options["data_sources"]
+        chunk_size = options.get("grants_chunk_size")
+        paginator = Paginator(grants.order_by("grant_id"), chunk_size)
 
-        for grant in grants:
-            if options["data_sources"]:
-                additional_data = generator.create(grant.data, options["data_sources"])
-            else:
-                additional_data = generator.create(grant.data)
+        # Update Grants in a separate function so the memory can be reclaimed between chunks
+        def update_grants_page(page_num):
+            page = paginator.page(page_num)
+            page_grants = page.object_list
 
-            grant.additional_data = additional_data
+            logger.info(
+                f"Updating Grants {page.start_index()}-{page.end_index()} of {paginator.count}"
+            )
 
-        Grant.objects.bulk_update(grants, ["additional_data"], batch_size=10000)
+            for grant in page_grants:
+                if data_sources:
+                    additional_data = generator.create(grant.data, data_sources)
+                else:
+                    additional_data = generator.create(grant.data)
+
+                grant.additional_data = additional_data
+
+            Grant.objects.bulk_update(page_grants, ["additional_data"])
+
+        for page_num in paginator.page_range:
+            update_grants_page(page_num)
+
+        logger.info(f"Updated {paginator.count} Grants Additional Data")
