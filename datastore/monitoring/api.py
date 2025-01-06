4e6cc4c5b6302d396389c10af7f546d93d1c8b96
Rikki Guy <rikki.guy@opendataservices.coop>
2025-01-06 13:09:26 +0000

    WIP

--- datastore/monitoring/api.py
+++ datastore/monitoring/api.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.db.models import Max, OuterRef, Subquery, Field, F
+from django.db.models import OuterRef, Subquery, ForeignKey, DO_NOTHING
 from rest_framework.generics import ListAPIView
 from rest_framework.settings import api_settings
 from rest_framework_csv.renderers import CSVRenderer
@@ -24,32 +24,52 @@ class SnapshotAPIView(ListAPIView):
         snapshot_date = self.kwargs.get("snapshot_date")
         queryset = super().get_queryset()
 
+        if snapshot_date is None:
+            snapshot_date = datetime.date.today()
+
+        # Find the latest snapshot on the given day, or earlier
+        snapshot_date_end_of_day = datetime.datetime.combine(
+            datetime.date.fromisoformat(snapshot_date), datetime.datetime.max.time()
+        )
+        filtered_queryset = queryset.filter(timestamp__lte=snapshot_date_end_of_day)
+
         ident_filter = {idf: OuterRef(idf) for idf in self.identifier_fields}
 
-        # If a snapshot date is given, find the latest snapshot on that day or earlier
-        if snapshot_date is not None:
-            snapshot_date_end_of_day = datetime.datetime.combine(
-                datetime.date.fromisoformat(snapshot_date), datetime.datetime.max.time()
-            )
-            queryset = queryset.filter(timestamp__lte=snapshot_date_end_of_day)
-
         # This is not the most efficient query, but it's only meant to be accessed occasionally, and still renders in ms
-        snapshot = (
-            queryset.filter(
-                pk__in=Subquery(
-                    queryset.filter(**ident_filter)
+        latest_record_pks = (
+            queryset.values(*self.identifier_fields)
+            .distinct(*self.identifier_fields)
+            .annotate(
+                # For each unique identifier, find the most recent timestamp in the filtered set
+                latest_record=Subquery(
+                    filtered_queryset.filter(**ident_filter)
                     .order_by("-timestamp")[:1]
-                    .values("pk")
+                    .values("pk"),
+                    output_field=ForeignKey(queryset.model, on_delete=DO_NOTHING),
                 )
             )
-            # We don't need to sort by identifiers but
-            # PostgreSQL requires order by fields must match distinct fields.
-            .order_by("-timestamp", *self.identifier_fields)
-            # Distinct to only return a single (latest) snapshot for each identifier.
-            .distinct("timestamp", *self.identifier_fields)
+            .values_list("latest_record", flat=True)
         )
 
+        snapshot = queryset.filter(pk__in=latest_record_pks)
         return snapshot
+
+        # snapshot = (
+        #    queryset.filter(
+        #        pk__in=Subquery(
+        #            queryset.filter(**ident_filter)
+        #            .order_by("-timestamp")[:1]
+        #            .values("pk")
+        #        )
+        #    )
+        #    # We don't need to sort by identifiers but
+        #    # PostgreSQL requires order by fields must match distinct fields.
+        #    .order_by("-timestamp", *self.identifier_fields)
+        #    # Distinct to only return a single (latest) snapshot for each identifier.
+        #    .distinct(*self.identifier_fields)
+        # )
+
+        # return snapshot
 
 
 class PublisherMetricsSnapshotAPIView(SnapshotAPIView):
