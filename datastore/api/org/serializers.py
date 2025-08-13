bfd6e928db579f5828265572f9073f83fd5bdc67
Michael Wood <michael.wood@opendataservices.coop>
2025-08-13 16:29:03 +0100

    monitoring/api: Update usage of aggregate entity data
    This ensures compatibility reflecting the structure change of aggregate
    data within the entities objects. The majority of the work is to combine
    the aggregate data back into not being split by recipient org/ind types.

--- datastore/api/org/serializers.py
+++ datastore/api/org/serializers.py
@@ -27,6 +27,8 @@ class OrganisationRefSerializer(DataclassSerializer):
 
 
 class OrganisationAggregateCurrencySerializer(serializers.Serializer):
+    # NOTE: avg to be deprecated in version 2
+    # https://github.com/ThreeSixtyGiving/datastore/issues/292
     avg = serializers.FloatField()
     max = serializers.FloatField()
     min = serializers.FloatField()
@@ -36,7 +38,120 @@ class OrganisationAggregateCurrencySerializer(serializers.Serializer):
 
 class OrganisationAggregateSerializer(serializers.Serializer):
     grants = serializers.IntegerField()
-    currencies = serializers.DictField(child=OrganisationAggregateCurrencySerializer())
+
+    currencies = serializers.SerializerMethodField()
+
+    # NOTE: This field will be deprecated in future version in favour of
+    # the direct representation from the entitiy model's aggregate data.
+    # This method is to provide API v1 compatibility.
+    def get_currencies(self, org):
+        def serialize_currencies(currencies):
+            """serialise the nested data using OrganisationAggregateCurrencySerializer"""
+            ret = {}
+            for currency in currencies.keys():
+                serialize = OrganisationAggregateCurrencySerializer(
+                    currencies[currency]
+                )
+                ret[currency] = serialize.data
+            return ret
+
+        def combine_currencies_org_ind(currency):
+            """recombine currency stats from both recipient_org and recipient_ind"""
+            combined_grants = (
+                recipient_org_currencies[currency]["grants"]
+                + recipient_ind_currencies[currency]["grants"]
+            )
+            combined_total = (
+                recipient_org_currencies[currency]["total"]
+                + recipient_ind_currencies[currency]["total"]
+            )
+
+            # Calculate the new average
+            if combined_grants > 0:
+                combined_avg = combined_total / combined_grants
+            else:
+                combined_avg = 0.0
+
+            # Determine the new min and max using direct key access
+            combined_min = min(
+                recipient_org_currencies[currency]["min"],
+                recipient_ind_currencies[currency]["min"],
+            )
+            combined_max = max(
+                recipient_org_currencies[currency]["max"],
+                recipient_ind_currencies[currency]["max"],
+            )
+            ret = {
+                "min": combined_min,
+                "max": combined_max,
+                "avg": combined_avg,
+                "grants": combined_grants,
+                "total": combined_total,
+            }
+            return ret
+
+        # Separate out the currencies from e.g.
+        # {
+        #  { GBP: { recipient_ind: { avg, max, min }, recipient_org: { avg, max, min },
+        #  { EUR: { recipient_ind: { avg, max, min },
+        # }
+        # and turn them into:
+        # recipient_org_currencies = { GBP : { avg, max, min .. }, USD: { avg, max, min ... } ..  }
+        # recipient_ind_currencies = { EUR : { avg, max, min ... },  ... }
+        recipient_org_currencies = {}
+        recipient_ind_currencies = {}
+
+        for currency in org["currencies"].keys():
+            if currency_stats := org["currencies"][currency].get("recipient_org"):
+                recipient_org_currencies[currency] = currency_stats
+
+            if currency_stats := org["currencies"][currency].get("recipient_ind"):
+                recipient_ind_currencies[currency] = currency_stats
+
+        # The easy cases (and most common) where the funder justs funds recipient_org_currencies
+        # return those currency stats
+        if recipient_org_currencies and not recipient_ind_currencies:
+            return serialize_currencies(recipient_org_currencies)
+
+        # Or only funds recipient_ind_currencies
+        # return those currency stats
+        if recipient_ind_currencies and not recipient_org_currencies:
+            return serialize_currencies(recipient_ind_currencies)
+
+        # If funds both inds and orgs we need to be aware of the possibility of any
+        # mix of currencies with any mix of ind or org recipients
+        if recipient_org_currencies and recipient_ind_currencies:
+            ret = {}
+
+            set_recipient_org_currencies = set(recipient_org_currencies.keys())
+            set_recipient_ind_currencies = set(recipient_ind_currencies.keys())
+
+            common_currencies = list(
+                set_recipient_org_currencies.intersection(set_recipient_ind_currencies)
+            )
+            currencies_only_in_recipient_org_currencies = list(
+                set_recipient_org_currencies.difference(set_recipient_ind_currencies)
+            )
+            currencies_only_in_recipient_ind_currencies = list(
+                set_recipient_ind_currencies.difference(set_recipient_org_currencies)
+            )
+
+            # Combine the combinations, either combining org/ind where they are
+            # in the same currency or when not extracting the stats and adding
+            # them to the output.
+
+            for currency in common_currencies:
+                ret[currency] = combine_currencies_org_ind(currency)
+
+            for currency in currencies_only_in_recipient_org_currencies:
+                ret[currency] = recipient_org_currencies[currency]
+
+            for currency in currencies_only_in_recipient_ind_currencies:
+                ret[currency] = recipient_ind_currencies[currency]
+
+            return serialize_currencies(ret)
+
+        raise Exception(f"The organisation didn't have the required data {org}")
 
 
 class OrganisationFunderSerializer(serializers.ModelSerializer):
